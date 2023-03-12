dd805d5f10c44c14c2c6361591e49a30e6b9e1a6
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-12 15:41:30 +1100

    styling for expense items

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './ExpenseItems.css'
 
 export default function ExpenseItems() {
+  const expenseDate = new Date(2022, 2, 12);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmpunt = 295.67;
   return (
-    <div>ExpenseItems</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className = 'expense-item__price'>{expenseAmpunt}</div>
+      </div>
+    </div>
   )
 }
+
+
