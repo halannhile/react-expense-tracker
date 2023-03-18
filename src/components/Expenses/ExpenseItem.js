e0c7ac701c9f32083c043b56309df9cfa5db56ec
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-19 10:34:33 +1100

    button event handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,9 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 export default function ExpenseItem(props) {
-  
+  const clickHandler = () => {
+    console.log('Clicked!')
+  };
 
   return (
     <Card className="expense-item">
@@ -12,6 +14,9 @@ export default function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className = 'expense-item__price'>{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>
+        Change title
+      </button>
     </Card>
   )
 }
