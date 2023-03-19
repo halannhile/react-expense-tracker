2990059a286545c4cccd9fbb371ca7cf7f69f06f
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-19 11:53:39 +1100

    form and event handlers

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import './NewExpense.css'
-import ExpenseForm
+import './NewExpense.css';
+import ExpenseForm from './ExpenseForm';
 
 export default function NewExpense() {
   return (
     <div className = "new-expense">
-        
+        <ExpenseForm/>
     </div>
   )
 }
