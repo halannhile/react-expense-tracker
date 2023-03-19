ff47bec52f339a567a569fbc30a29f9e76f535c4
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-19 15:26:49 +1100

    two-way binding for clearing form post submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -12,7 +12,6 @@ export default function ExpenseForm() {
     //     enteredDate: ''
     // });
 
-    
     // const titleChangeHandler = (event) => {
     //     setUserInput((prevState) => {
     //         return { ...prevState, enteredTitle: event.target.value };        
@@ -56,6 +55,9 @@ export default function ExpenseForm() {
         };
 
         console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
 
   return (
@@ -63,17 +65,27 @@ export default function ExpenseForm() {
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" value = {enteredTitle} onChange={titleChangeHandler}/>
+                <input type="text"
+                 value = {enteredTitle} 
+                 onChange={titleChangeHandler}/>
             </div>
 
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min = "0.01" step="0.01" onChange={amountChangeHandler}/>
+                <input type="number" 
+                value = {enteredAmount}
+                min = "0.01" 
+                step="0.01" 
+                onChange={amountChangeHandler}/>
             </div>
 
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min = "2019-01-01" max = "2025-12-31" onChange={dateChangeHandler}/>
+                <input type="date" 
+                value = {enteredDate}
+                min = "2019-01-01" 
+                max = "2025-12-31" 
+                onChange={dateChangeHandler}/>
             </div>
         </div>
         <div className="new-expense__action">
