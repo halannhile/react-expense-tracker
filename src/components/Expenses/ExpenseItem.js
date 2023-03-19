c8ee5d0b22b3e917355bc2082dedd6c426378c16
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-19 11:08:12 +1100

    hooks for changing title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 export default function ExpenseItem(props) {
-
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    title = 'Updated!';
+    // state updating function
+    // when triggered, will not only assign new value to a variable
+    // but also trigger the Component function to be run again
+    setTitle('Updated!');
     console.log(title);
   };
 
   return (
+    // component function: 
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
