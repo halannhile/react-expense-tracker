653b1d40779083813f8db6e0962fac279570c6de
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-20 22:13:30 +1100

    removed button from ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,13 +6,13 @@ import Card from '../UI/Card';
 export default function ExpenseItem(props) {
   const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    // state updating function
-    // when triggered, will not only assign new value to a variable
-    // but also trigger the Component function to be run again
-    setTitle('Updated!');
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   // state updating function
+  //   // when triggered, will not only assign new value to a variable
+  //   // but also trigger the Component function to be run again
+  //   setTitle('Updated!');
+  //   console.log(title);
+  // };
 
   return (
     // component function: 
@@ -22,9 +22,9 @@ export default function ExpenseItem(props) {
         <h2>{title}</h2>
         <div className = 'expense-item__price'>{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>
+      {/* <button onClick={clickHandler}>
         Change title
-      </button>
+      </button> */}
     </Card>
   )
 }
