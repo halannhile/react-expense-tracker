53cefd29aa8edabe187d8d8f19596c6d29cc68de
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-22 22:23:02 +1100

    click handler for setTitle

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,6 +6,15 @@ import Card from '../UI/Card';
 
 export default function ExpenseItem(props) {
 
+  // const [title, setTitle] = useState(props.title);
+
+  // const clickHandler = () => {
+  //   // state updating function
+  //   // when triggered, will not only assign new value to a variable
+  //   // but also trigger the Component function to be run again
+  //   setTitle('Updated!');
+  //   console.log(title);
+  // };
 
   return (
     // component function: 
