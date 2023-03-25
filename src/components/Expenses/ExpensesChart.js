892a382ba3f22deeb5a40c87590b4c82daed075d
Nhi (Chelsea) Le <halannhile@gmail.com>
2023-03-25 16:31:30 +1100

    finished ExpensesChart.js

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Chart from '../Chart/Chart'
 
+// props will be filteredExpenses here since we want to display chart by month for each year
+
 export default function ExpensesChart(props) {
     // chartDataPoint is an array of objects
     // because Chart receives props as an object
-    const chartDataPoint = [
+    const chartDataPoints = [
         { label: 'Jan', value:0 },
         { label: 'Feb', value:0 },
         { label: 'Mar', value:0 },
@@ -17,9 +19,19 @@ export default function ExpensesChart(props) {
         { label: 'Oct', value:0 },
         { label: 'Nov', value:0 },
         { label: 'Dec', value:0 },
-    ]
+    ];
+
+    // loop through each expense item in filteredExpenses
+    // add expense amount to is respective month
+    for (const expense in props.expenses) {
+        const expenseMonth = expense.date.getMonth(); // Jan = 0
+        chartDataPoints[expenseMonth].value += expense.amount;
+    }
+
+    // at this point, chartDataPoints has been updated with total value of expense for each month
+
     return (
-        <Chart/>
+        <Chart dataPoints={chartDataPoints} />
     )
 }
 
